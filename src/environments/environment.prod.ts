245e27672d70f818422a04c4cbb0d138248e0975
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-26 17:09:28 +0100

    Changing the firestore database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   
   firebase : {
-    apiKey: "AIzaSyBU3YSUEFt9eLa-VxBDBUDUjhOuwP3CCbY",
-    authDomain: "gestion-client-57fba.firebaseapp.com",
-    databaseURL: "https://gestion-client-57fba.firebaseio.com",
-    projectId: "gestion-client-57fba",
-    storageBucket: "gestion-client-57fba.appspot.com",
-    messagingSenderId: "627011346363"
+    apiKey: "AIzaSyCwZbNlQi4ofv-JoiNmOY7er_2CfJCFvhE",
+    authDomain: "gestion-client-aeaee.firebaseapp.com",
+    databaseURL: "https://gestion-client-aeaee.firebaseio.com",
+    projectId: "gestion-client-aeaee",
+    storageBucket: "gestion-client-aeaee.appspot.com",
+    messagingSenderId: "1000826572238"
   }
 };
