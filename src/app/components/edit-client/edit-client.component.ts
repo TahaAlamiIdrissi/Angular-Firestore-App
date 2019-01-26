0eca504071d80276b097b11ff9b7c8b152d0f2d8
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-26 17:16:01 +0100

    show the client who has been choisen and updating his data

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClientService } from 'src/app/services/client.service';
+import { Client } from 'src/app/client';
 
 @Component({
   selector: 'app-edit-client',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditClientComponent implements OnInit {
 
-  constructor() { }
+  client : Client={
+    id:"",
+    firstName:"",
+    lastName:"",
+    email:"",
+    phone:"",
+    balance:0,
+    active : false
+  }
+  constructor(private route:ActivatedRoute,
+              private router:Router,
+              private clientService:ClientService) { }
+  id : "";
 
   ngOnInit() {
+     this.id=this.route.snapshot.params.id;
+    this.clientService._getClient(this.id)
+                      .subscribe((client:Client)=> this.client=client);
   }
 
+  updateClient(form){
+    if(form.valid){
+      this.client.id=this.id;
+    this.clientService.updateClient(this.client)
+                      .then(()=>this.router.navigate(['/']))
+                      .catch((err)=>console.error(err));
+                    }
+                  }
 }
