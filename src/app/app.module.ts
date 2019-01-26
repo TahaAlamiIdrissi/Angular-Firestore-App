e6cca64c37b6848a40e40584cc9ccdaf03653e61
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-26 11:23:16 +0100

    added new Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { ShowClientComponent } from './components/show-client/show-client.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ListsClientComponent } from './components/lists-client/lists-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddClientComponent,
+    EditClientComponent,
+    ShowClientComponent,
+    PageNotFoundComponent,
+    ListsClientComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lists-client/lists-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lists-client',
+  templateUrl: './lists-client.component.html',
+  styleUrls: ['./lists-client.component.css']
+})
+export class ListsClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
