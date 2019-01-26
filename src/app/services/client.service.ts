0b3b104c747eaefef90cff1038649ddb361f93e8
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-26 17:10:24 +0100

    creating the crud using the nosql format collection . doc

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Client } from '../client';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +15,29 @@ export class ClientService {
    }
 
    _getClients(){
-    return  this.clientCollection.valueChanges();
+    return  this.clientCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Client;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      })));
+   }
+   _storeClient(data){
+     //data : data to add
+     return this.clientCollection.add(data);
+     //adding data clients in Firestore
+   }
+   
+   _deleteClient(id){
+     return this.clientCollection.doc(id).delete();
+   }
+   _toggleStatus(data,id){
+     return this.clientCollection.doc(id).update(data);
+   }
+   _getClient(id){
+     return this.clientCollection.doc(id).valueChanges();//get all the object
+   }
+   updateClient(client){
+     return this.clientCollection.doc(client.id).update(client);
    }
 }
