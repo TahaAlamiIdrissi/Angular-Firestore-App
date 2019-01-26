d7b9d3b22a7ce7ba7a7cf0c815e4efe8d2152038
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-26 12:08:45 +0100

    injecting AngularFirestore into client service to establish communication

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
-
-  constructor() { }
+  //afs: angularFireStore
+  constructor(private afs:AngularFirestore) { }
 }
