192ace51cfc95d473107cbddc702b1ee9df605f7
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-26 17:18:36 +0100

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListsClientComponent } from './components/lists-client/lists-client.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :"" , component : ListsClientComponent},
+  {path :"add" , component : AddClientComponent},
+  {path :"edit/:id" , component : EditClientComponent},
+  {path :"**" , component : PageNotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
