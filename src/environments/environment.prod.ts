56bfae94ca958564f7198d93cddea37a1a6d6289
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-27 13:24:32 +0100

    changes ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   
   firebase : {
-    apiKey: "please enter your approrpiate API KEY",
-    authDomain: "gestion-client-aeaee.firebaseapp.com",
-    databaseURL: "https://gestion-client-aeaee.firebaseio.com",
-    projectId: "gestion-client-aeaee",
-    storageBucket: "gestion-client-aeaee.appspot.com",
-    messagingSenderId: "1000826572238"
+    apiKey: "AIzaSyBU3YSUEFt9eLa-VxBDBUDUjhOuwP3CCbY",
+    authDomain: "gestion-client-57fba.firebaseapp.com",
+    databaseURL: "https://gestion-client-57fba.firebaseio.com",
+    projectId: "gestion-client-57fba",
+    storageBucket: "gestion-client-57fba.appspot.com",
+    messagingSenderId: "627011346363"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
   production: false,
   
   firebase : {
-    apiKey: "please enter your appropriate API KEY",
-    authDomain: "gestion-client-aeaee.firebaseapp.com",
-    databaseURL: "https://gestion-client-aeaee.firebaseio.com",
-    projectId: "gestion-client-aeaee",
-    storageBucket: "gestion-client-aeaee.appspot.com",
-    messagingSenderId: "1000826572238"
+    apiKey: "AIzaSyBU3YSUEFt9eLa-VxBDBUDUjhOuwP3CCbY",
+    authDomain: "gestion-client-57fba.firebaseapp.com",
+    databaseURL: "https://gestion-client-57fba.firebaseio.com",
+    projectId: "gestion-client-57fba",
+    storageBucket: "gestion-client-57fba.appspot.com",
+    messagingSenderId: "627011346363"
   }
 };
 
