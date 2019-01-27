1ebc6e40bc8fb1b8a06463bdb9373bfc6a378342
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-27 13:10:07 +0100

    adding the logout to the navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  currentUser = null;
+
+  constructor(private authService : AuthService,
+              private router : Router) { }
 
   ngOnInit() {
+    this.authService._isAuthenticated()
+                    .subscribe(user => this.currentUser = user);
+  }
+
+  logout(){
+    this.authService._logout()
+                    .then(() => {
+                      this.router.navigate(['/login'])
+                    });
   }
 
 }
