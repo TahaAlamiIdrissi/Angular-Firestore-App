89276dcc060d733a86c1c7f0b85c97329d52a7bf
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-27 13:08:28 +0100

    Implementing the login logout register  isauthenticated

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) { }
+
+  _register(email,password){
+      return this.afAuth.auth.createUserWithEmailAndPassword(email,password);//return a promise
+  }
+
+  _login(email,password){
+    return this.afAuth.auth.signInWithEmailAndPassword(email,password);//return a promise
+  }
+  _logout(){
+    return this.afAuth.auth.signOut();//return a promise
+  }
+  _isAuthenticated(){
+    return this.afAuth.user;// return an observable
+  }
 }
