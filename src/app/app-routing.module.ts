b1b7ab1766adb676284ac502827500e972cbf5a0
TahaAlamiIdrissi <miala69@hotmail.fr>
2019-01-27 13:15:06 +0100

    adding the guard canActivate to some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { ListsClientComponent } from './components/lists-client/lists-client.com
 import { AddClientComponent } from './components/add-client/add-client.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  {path :"" , component : ListsClientComponent},
-  {path :"add" , component : AddClientComponent},
-  {path :"edit/:id" , component : EditClientComponent},
+  {path :"" , component : ListsClientComponent,canActivate:[AuthGuard]},
+  {path :"login" , component : LoginComponent},
+  {path :"register" , component : RegisterComponent},
+  {path :"add" , component : AddClientComponent,canActivate:[AuthGuard]},
+  {path :"edit/:id" , component : EditClientComponent,canActivate:[AuthGuard]},
   {path :"**" , component : PageNotFoundComponent},
 ];
 
